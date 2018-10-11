b4d2e0e3d4e3ad63c5084f6494d9fe8eaf04e3c3
Jonas Wagner <jwagner@life.uni-leipzig.de>
2018-10-11 17:07:38 +0200

    Make WebSocket Timeout Adjustable

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 sed -i "s#<search-store-root>#${SEARCH_STORE_ROOT}#" /etc/nginx/nginx.conf
+sed -i "s#<websocket-timeout-seconds>#${WEBSOCKET_TIMEOUT_SECONDS:-30}#" /etc/nginx/nginx.conf
 sed -i "s#<mdr-root>#${MDR_ROOT}#" /flags.js
 sed -i "s#<mdr-namespace>#${MDR_NAMESPACE}#" /flags.js
 sed -i -f /flags.sed /usr/share/nginx/html/index.html
